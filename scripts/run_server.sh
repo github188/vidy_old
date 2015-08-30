c042fa08fffa3b8dc6ea0454a37e4176fa244cd9
LaurenLuoYun <lauren.luo@extremevision.mo>
2015-08-30 11:18:12 +0800

    update

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-/root/vidy/bin/Vidy CTCCHZ 8 http://120.55.117.62:8090/flv8.flv 1 &
-/root/vidy/bin/Vidy CTCCHZ 9 http://120.55.117.62:8090/flv9.flv 1 &
-/root/vidy/bin/Vidy CTCCHZ 15 http://120.55.117.62:8090/flv15.flv 1 &
-/root/vidy/bin/Vidy CTCCHZ 16 http://120.55.117.62:8090/flv16.flv 1 &
-/root/vidy/bin/Vidy CTCCHZ 21 http://120.55.117.62:8090/flv21.flv 1 &
-/root/vidy/bin/Vidy CTCCHZ 22 http://120.55.117.62:8090/flv22.flv 1 &
-/root/vidy/bin/Vidy CTCCHZ 23 http://120.55.117.62:8090/flv23.flv 1 &
-/root/vidy/bin/Vidy CTCCHZ 25 http://120.55.117.62:8090/flv25.flv 2 &
+/usr/local/vidy/bin/Vidy CTCCHZ 8 http://10.173.164.13:8090/flv8.flv 1 &
+/usr/local/vidy/bin/Vidy CTCCHZ 9 http://10.173.164.13:8090/flv9.flv 1 &
+/usr/local/vidy/bin/Vidy CTCCHZ 15 http://10.173.164.13:8090/flv15.flv 1 &
+/usr/local/vidy/bin/Vidy CTCCHZ 16 http://10.173.164.13:8090/flv16.flv 1 &
+/usr/local/vidy/bin/Vidy CTCCHZ 21 http://10.173.164.13:8090/flv21.flv 1 &
+/usr/local/vidy/bin/Vidy CTCCHZ 22 http://10.173.164.13:8090/flv22.flv 1 &
+/usr/local/vidy/bin/Vidy CTCCHZ 23 http://10.173.164.13:8090/flv23.flv 1 &
+/usr/local/vidy/bin/Vidy CTCCHZ 25 http://10.173.164.13:8090/flv25.flv 2 &
 
