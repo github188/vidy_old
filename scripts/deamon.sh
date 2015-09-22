1bb333c8cb0fd65185d97da0aeedf2f3097cf315
System Administrator <root@luoyuns-MBP.lan>
2015-09-22 08:17:18 +0800

    update

--- scripts/deamon.sh
+++ scripts/deamon.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 path="/usr/local/vidy/bin/Vidy"
 while:
-do $path CTCCHZ $1 http://10.173.164.13:8090/flv$1.flv $2 &
+do 
+$path CTCCHZ $1 http://10.173.164.13:8090/flv$1.flv $2 &
 sleep 100
 done
--- scripts/deamon_save2db.sh
+++ scripts/deamon_save2db.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 path="/usr/local/vidy/save2db/bin/Vidy_Save2DB"
 while:
-do $path CTCCHZ $1 $2 &
+do 
+$path CTCCHZ $1 $2 &
 sleep 100
 done
